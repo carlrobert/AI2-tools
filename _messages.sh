8aa35f36adf5d3f9c697743b7629de2dba4f4d6e
Robert Storlind <carlrobert@users.noreply.github.com>
2016-05-06 10:29:47 +0200

    Update _messages.sh

--- _messages.sh
+++ _messages.sh
@@ -1,4 +1,4 @@
-curl -O http://pootle.appinventor.mit.edu/download/sv/app-inventor/_messages.po -L --proxy https://23048519:mn6aBoxV@proxy.global.sonyericsson.net:8080
+curl -O http://pootle.appinventor.mit.edu/download/sv/app-inventor/_messages.po -L
 
 po2prop -t 2-prop.properties --personality=java-utf8 _messages.po -o test2
 
