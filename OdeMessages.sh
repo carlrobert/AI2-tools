464e5a00ad73ced6e5bc4ff8e6461ccecdb4ff25
Robert Storlind <carlrobert@users.noreply.github.com>
2016-05-20 10:17:15 +0200

    Update OdeMessages.sh

--- OdeMessages.sh
+++ OdeMessages.sh
@@ -1,5 +1,15 @@
 #!
-# Update OdeMessages_sv.properties based on the latest pootle translation (OdeMessages.po) 
-curl -LO http://pootle.appinventor.mit.edu/download/sv/app-inventor/OdeMessages.po
-curl -LOk https://github.com/mit-cml/appinventor-sources/raw/master/appinventor/appengine/src/com/google/appinventor/client/OdeMessages_sv.properties
-po2prop -t OdeMessages_sv.properties --personality=java-utf8 OdeMessages.po -o OdeMessages_sv.properties.NEW
+# _messages.sh
+# Update _messages.js based on the latest pootle translation (OdeMessages.po)
+FILE=_messages.js
+curl -LO http://pootle.appinventor.mit.edu/download/sv/app-inventor/_messages.po
+curl -LOk https://github.com/mit-cml/appinventor-sources/raw/master/appinventor/blocklyeditor/src/msg/sv/$FILE
+
+# Disassemble into constituent parts: preamble, body, postamble
+python3 js2prop.py $FILE
+
+# Process the body containing the messages subject to translation
+po2prop -t 2-$FILE.properties --personality=java-utf8 _messages.po | python3 prop2js.py > 2-$FILE.properties.NEW
+
+# Reassemble
+cat 1-pre-$FILE 2-$FILE.properties.NEW 3-post-$FILE > $FILE.NEW
